4f05b3648ea90e20a401ca65dbb5de3e61d3bc79
chobs95 <charlieroberts201@hotmail.co.uk>
2024-11-21 17:01:56 +0000

    changes

--- main.py
+++ main.py
@@ -136,7 +136,10 @@ class TechFighters:
             self.screen.blit(score_text, score_rect)  # Draw text at the center of the screen
             pygame.display.flip()
             pygame.time.delay(3000)  # Pause for 3 seconds before exiting
-
+            self.distance = 0
+            self.user.rect.y = 1
+            self.user.moving_left = False
+            self.user.moving_right = False
             self.game_active = False
     
 if __name__ == '__main__':
