578c1f3e3c0b5a76ccf8a64f61601e51541a61c7
Alexia Maynart <alexia.maynart@gmail.com>
2024-11-21 12:25:14 +0000

    Added Obstacle class

--- settings.py
+++ settings.py
@@ -3,3 +3,11 @@ class Settings:
         self.screen_width = 560
         self.screen_height = 900
         self.bg_colour = (135, 206, 235)
+
+
+        # Obstacle settings = bugs ythe player has to avoid
+        self.obstacle_speed = 2.0
+        self.obstacle_width = 10
+        self.obstacle_height = 10 
+        self.obstacle_colour = (60, 60, 60)
+        
